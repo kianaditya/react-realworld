19e0e74c374b40ec463456674e204171e838d0b2
Aditya Naik <naik.aditya@gmail.com>
2019-11-21 12:26:18 +0100

    Renders articles List properly

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Banner from "./Banner";
 const axios = require("axios");
 
 const ArticleList = () => {
@@ -14,12 +15,89 @@ const ArticleList = () => {
   }, []);
   const renderArticles = articles.map((article, index) => {
     return (
-      <>
-        <div key={index}>{article.title}</div>
-      </>
+      <div className="article-preview">
+        <div className="article-meta">
+          <a href="profile.html">
+            <img src={article.author.image} />
+          </a>
+          <div className="info">
+            <a href="" className="author">
+              {article.author.userName}
+            </a>
+            <span className="date">{article.createdAt}</span>
+          </div>
+          <button className="btn btn-outline-primary btn-sm pull-xs-right">
+            <i className="ion-heart"></i> {article.favoritesCount}
+          </button>
+        </div>
+        <a href="" className="preview-link">
+          <h1>{article.title}</h1>
+          <p>{article.description}</p>
+          <span>Read more...</span>
+        </a>
+      </div>
     );
   });
-  return <div data-cy="allArticles">{renderArticles}</div>;
+  return (
+    <>
+      <div className="home-page" data-cy="allArticles">
+        <Banner />
+        <div className="container page">
+          <div className="row">
+            <div className="col-md-9">
+              <div className="feed-toggle">
+                <ul className="nav nav-pills outline-active">
+                  <li className="nav-item">
+                    <a className="nav-link disabled" href="">
+                      Your Feed
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link active" href="">
+                      Global Feed
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              {renderArticles}
+            </div>
+            <div className="col-md-3">
+              <div className="sidebar">
+                <p>Popular Tags</p>
+
+                <div className="tag-list">
+                  <a href="" className="tag-pill tag-default">
+                    programming
+                  </a>
+                  <a href="" className="tag-pill tag-default">
+                    javascript
+                  </a>
+                  <a href="" className="tag-pill tag-default">
+                    emberjs
+                  </a>
+                  <a href="" className="tag-pill tag-default">
+                    angularjs
+                  </a>
+                  <a href="" className="tag-pill tag-default">
+                    react
+                  </a>
+                  <a href="" className="tag-pill tag-default">
+                    mean
+                  </a>
+                  <a href="" className="tag-pill tag-default">
+                    node
+                  </a>
+                  <a href="" className="tag-pill tag-default">
+                    rails
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ArticleList;
