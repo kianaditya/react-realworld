e48a36edc4742e024038f50a7d083b31f1f166d4
Aditya Naik <naik.aditya@gmail.com>
2019-11-23 10:57:20 +0100

    Adds login specs, adds LoginForm component

---
+++ cypress/integration/auth/user_can_login_spec.js
@@ -0,0 +1,22 @@
+/// <reference types="Cypress" />
+describe("User can login", () => {
+  it("User can successfully login", () => {
+    // cy.route({
+    //   method: "POST",
+    //   url: "https://conduit.productionready.io/api/users",
+    //   response: "fixture:successful_registration.json"
+    // });
+    cy.visit("/");
+    cy.get("[data-cy=loginLink]").click();
+    cy.url().should("contain", "/login");
+    [
+      { field: "userName", text: "TestAccount" },
+      { field: "email", text: "test@mail.com" },
+      { field: "password", text: "password" }
+    ].forEach(element => {
+      cy.get(`[data-cy=${element.field}]`).type(element.text);
+    });
+    cy.get("[data-cy=loginButton]").click();
+    cy.url().should("contain", "/");
+  });
+});
