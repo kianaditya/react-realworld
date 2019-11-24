1ae6e1a772392450f381fba0ca17e53b68718a7d
Aditya Naik <naik.aditya@gmail.com>
2019-11-24 17:09:04 +0100

    sets up context API,Adds usecontext to propagate state across the component tree

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import {Switch, Route} from "react-router-dom"
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import ArticleList from './components/ArticleList';
-import RegistrationForm from './components/RegistrationForm';
-import LoginForm from './components/LoginForm';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ArticleList from "./components/ArticleList";
+import RegistrationForm from "./components/RegistrationForm";
+import LoginForm from "./components/LoginForm";
+import { AppContextProvider } from "./AppContext";
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Switch>
-        <Route exact path='/' render={() => <ArticleList/>}/>
-        <Route path='/signup' render={() =><RegistrationForm/>}/>
-        <Route path='/login' render={() =><LoginForm/>}/>
-      </Switch>
-      <Footer/>
-    </div>
+    <AppContextProvider>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" render={() => <ArticleList />} />
+          <Route path="/signup" render={() => <RegistrationForm />} />
+          <Route path="/login" render={() => <LoginForm />} />
+        </Switch>
+        <Footer />
+      </div>
+    </AppContextProvider>
   );
 }
 
