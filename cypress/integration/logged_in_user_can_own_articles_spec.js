38aa801e7bbb42c64f72c8a2b38358530b49043a
Aditya Naik <naik.aditya@gmail.com>
2020-01-19 20:25:46 +0100

    Adds test, fixture, updates logged in as commands

---
+++ cypress/integration/logged_in_user_can_own_articles_spec.js
@@ -0,0 +1,28 @@
+/// <reference types="Cypress" />
+describe("Logged in User can see own articles on Homepage", () => {
+  it("logged in User can see own articles", () => {
+    cy.route({
+      method: "GET",
+      url: "https://conduit.productionready.io/api/articles",
+      status: 200,
+      response: "fixture:article_list.json"
+    });
+    cy.route({
+      method: "GET",
+      url: "https://conduit.productionready.io/api/tags",
+      status: 200,
+      response: "fixture:tags.json"
+    });
+    cy.route({
+      method:"GET",
+      url:"https://conduit.productionready.io/api/articles/feed?limit=10&offset=0",
+      status: 200,
+      response: "fixture:own_article_list.json"
+    })
+    cy.loggedInAs('user121212@mail.com')
+    cy.get("[data-cy=allArticles]").should(
+      "contain.text",
+      "How to train your dragon"
+    );
+  });
+});
