d8df0035b8c50e0ae42e208440c713380ca08bb9
Aditya Naik <naik.aditya@gmail.com>
2020-01-31 16:11:36 +0100

    Adds logout test to increase coverage

--- cypress/integration/auth/user_can_login_spec.js
+++ cypress/integration/auth/user_can_login_spec.js
@@ -1,6 +1,6 @@
 /// <reference types="Cypress" />
 describe("User can login", () => {
-  it("User can successfully login", () => {
+  it("User can successfully login and logout", () => {
     cy.route({
       method: "POST",
       url: "https://conduit.productionready.io/api/users/login",
@@ -18,5 +18,8 @@ describe("User can login", () => {
     cy.get("[data-cy=login-button]").click();
     cy.url().should("not.contain", "/login");
     cy.get("[data-cy=loginLink]").should("not.exist");
+    cy.get("[data-cy=settings]").click();
+    cy.get("[data-cy=logout]").click();
+    cy.get("[data-cy=loginLink]").should("exist");
   });
 });
