cbb287da79c2ff887d2c80ec9310ea65daca65ee
Aditya <naik.aditya@gmail.com>
2020-11-26 14:24:39 +0100

    Updates api Url for app and for cypress, starts converting `cy.route` to `cy.intercept`

--- cypress/integration/user_can_see_articles_on_homepage_spec.js
+++ cypress/integration/user_can_see_articles_on_homepage_spec.js
@@ -1,22 +1,10 @@
 /// <reference types="Cypress" />
-describe("User can see articles on Homepage", () => {
-  it("User can see all articles", () => {
-    cy.route({
-      method: "GET",
-      url: "https://conduit.productionready.io/api/articles",
-      status: 200,
-      response: "fixture:article_list.json"
-    });
-    cy.route({
-      method: "GET",
-      url: "https://conduit.productionready.io/api/tags",
-      status: 200,
-      response: "fixture:tags.json"
-    });
-    cy.visit("/");
-    cy.get("[data-cy=allArticles]").should(
-      "contain.text",
-      "How to train your dragon"
-    );
-  });
-});
+describe('User can see articles on Homepage', () => {
+  it('User can see all articles', () => {
+    cy.visit('/')
+    cy.get('[data-cy=allArticles]').should(
+      'contain.text',
+      'How to train your dragon'
+    )
+  })
+})
