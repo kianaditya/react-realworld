81e86ab51353caf1016041e83e3f8e7d55f60497
Aditya <naik.aditya@gmail.com>
2020-11-26 14:27:57 +0100

    test fixed - user can update profile

--- cypress/integration/user_can_update_profile_spec.js
+++ cypress/integration/user_can_update_profile_spec.js
@@ -1,33 +1,26 @@
 /// <reference types="Cypress" />
+const apiUrl = Cypress.env('apiUrl')
 
-describe("User can update profile", () => {
-  it("User can successfully update profile", () => {
-    cy.route({
-      method: "POST",
-      status: 200,
-      url: "https://conduit.productionready.io/api/users/login",
-      response: "fixture:successful_login.json"
-    });
-    cy.route({
-      method: "PUT",
-      status: 200,
-      url: "https://conduit.productionready.io/api/user",
-      response: "fixture:successful_user_update.json"
-    });
-    cy.login();
-    cy.get("[data-cy=settings]").click();
-    cy.url().should("contain", "/settings");
-    [
-      { field: "email", text: "test1@mail.com" },
-      { field: "password", text: "password1" },
-      { field: "password", text: "password1" },
-      { field: "username", text: "username1" },
-      { field: "bio", text: "bio1" },
-      { field: "image", text: "http://localhost:3000" }
-    ].forEach(element => {
-      cy.get(`[data-cy=${element.field}]`).type(element.text);
-    });
-    cy.get("[data-cy=updateProfile]").click();
-    cy.url().should("not.contain", "/settings");
-  });
-});
+describe('User can update profile', () => {
+  it('User can successfully update profile', () => {
+    cy.intercept('PUT', `${apiUrl}user`, {
+      statusCode: 200,
+      fixture: 'successful_user_update.json',
+    })
+    cy.login()
+    cy.get('[data-cy=settings]').click()
+    cy.url().should('contain', '/settings')
+    ;[
+      { field: 'email', text: 'test1@mail.com' },
+      { field: 'password', text: 'password1' },
+      { field: 'password', text: 'password1' },
+      { field: 'username', text: 'username1' },
+      { field: 'bio', text: 'bio1' },
+      { field: 'image', text: 'http://localhost:3000' },
+    ].forEach((element) => {
+      cy.get(`[data-cy=${element.field}]`).type(element.text)
+    })
+    cy.get('[data-cy=updateProfile]').click()
+    cy.url().should('not.contain', '/settings')
+  })
+})
