d67079231868e5466710c0d853c5edf6b3c69404
Aditya <naik.aditya@gmail.com>
2020-11-26 15:48:47 +0100

    Updates Auth tests

--- cypress/integration/auth/user_can_login_spec.js
+++ cypress/integration/auth/user_can_login_spec.js
@@ -1,25 +1,25 @@
 /// <reference types="Cypress" />
-describe("User can login", () => {
-  it("User can successfully login and logout", () => {
-    cy.route({
-      method: "POST",
-      url: "https://conduit.productionready.io/api/users/login",
-      response: "fixture:successful_login.json"
-    });
-    cy.visit("/");
-    cy.get("[data-cy=loginLink]").click();
-    cy.url().should("contain", "/login");
-    [
-      { field: "email", text: "test@mail.com" },
-      { field: "password", text: "password" }
-    ].forEach(element => {
-      cy.get(`[data-cy=${element.field}]`).type(element.text);
-    });
-    cy.get("[data-cy=login-button]").click();
-    cy.url().should("not.contain", "/login");
-    cy.get("[data-cy=loginLink]").should("not.exist");
-    cy.get("[data-cy=settings]").click();
-    cy.get("[data-cy=logout]").click();
-    cy.get("[data-cy=loginLink]").should("exist");
-  });
-});
+const apiUrl = Cypress.env('apiUrl')
+
+describe('User can login', () => {
+  it('User can successfully login and logout', () => {
+    cy.intercept('POST', `${apiUrl}users/login`, {
+      fixture: 'successful_login.json',
+    })
+    cy.visit('/')
+    cy.get('[data-cy=loginLink]').click()
+    cy.url().should('contain', '/login')
+    ;[
+      { field: 'email', text: 'test@mail.com' },
+      { field: 'password', text: 'password' },
+    ].forEach((element) => {
+      cy.get(`[data-cy=${element.field}]`).type(element.text)
+    })
+    cy.get('[data-cy=login-button]').click()
+    cy.url().should('not.contain', '/login')
+    cy.get('[data-cy=loginLink]').should('not.exist')
+    cy.get('[data-cy=settings]').click()
+    cy.get('[data-cy=logout]').click()
+    cy.get('[data-cy=loginLink]').should('exist')
+  })
+})
