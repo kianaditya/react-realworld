95ed0f535bb2dde8e7febb7a46ea8ba5e3751f85
Aditya <naik.aditya@gmail.com>
2020-11-27 09:29:50 +0100

    Update user profile spec

--- cypress/integration/user_profile_spec.js
+++ cypress/integration/user_profile_spec.js
@@ -1,37 +1,56 @@
 ///  <reference types="Cypress" />
 
-describe("User can visit user profile", () => {
+const apiUrl = Cypress.env('apiUrl')
+
+describe('User can visit user profile', () => {
   beforeEach(() => {
-    cy.login();
-  });
-  it("user can visit profile", () => {
-    cy.route({
-      method: "GET",
-      url: "https://conduit.productionready.io/api/profiles/test@mail.com",
-      status: 200,
-      response: "fixture:profile.json"
-    });
-    cy.route({
-      method: "GET",
-      url: "https://conduit.productionready.io/api/articles?author=test@mail.com",
-      status: 200,
-      response: "fixture:article_list.json"
-    });
-    cy.route({
-      method: "GET",
-      url: "https://conduit.productionready.io/api/articles?favorited=test@mail.com",
-      status: 200,
-      response: "fixture:own_article_list.json"
-    });
-    cy.get("[data-cy=profile]").click();
-    cy.url().should("contain","/profile/test@mail.com")
-    cy.get("[data-cy=profile-to-settings]").click()
-    cy.url().should("contain", "/settings");
-    cy.get("[data-cy=profile]").click();
-    cy.url().should("contain","/profile/test@mail.com")
-    cy.get("[data-cy=fav-articles]").click();
-    cy.get("[data-cy=article-title]").last().should("contain.text","How to train your dragon 2")
-    cy.get("[data-cy=my-articles]").click();
-    cy.get("[data-cy=article-title]").last().should("contain.text","How to your dragon 2")
-  });
-});
+    cy.login()
+  })
+  it('user can visit profile', () => {
+    cy.intercept('GET', `${apiUrl}articles`, {
+      statusCode: 200,
+      fixture: 'article_list.json',
+    })
+    cy.intercept('GET', `${apiUrl}tags`, {
+      statusCode: 200,
+      fixture: 'tags.json',
+    })
+    cy.intercept('GET', `${apiUrl}articles/feed`, {
+      statusCode: 200,
+      fixture: 'own_article_list.json',
+    })
+    cy.intercept('GET', `${apiUrl}profiles/test@mail.com`, {
+      statusCode: 200,
+      fixture: 'profile.json',
+    })
+
+    cy.intercept(
+      { url: `${apiUrl}articles`, query: { author: 'test@mail.com' } },
+      {
+        statusCode: 200,
+        fixture: 'article_list.json',
+      }
+    )
+    cy.intercept(
+      { url: `${apiUrl}articles`, query: { favorited: 'test@mail.com' } },
+      {
+        statusCode: 200,
+        fixture: 'article_list.json',
+      }
+    )
+    cy.get('[data-cy=profile]').click()
+    cy.url().should('contain', '/profile/test@mail.com')
+    cy.get('[data-cy=profile-to-settings]').click()
+    cy.url().should('contain', '/settings')
+    cy.get('[data-cy=profile]').click()
+    cy.url().should('contain', '/profile/test@mail.com')
+    cy.get('[data-cy=fav-articles]').click()
+    cy.get('[data-cy=article-title]')
+      .last()
+      .should('contain.text', 'How to your dragon 2')
+    cy.get('[data-cy=my-articles]').click()
+    cy.get('[data-cy=article-title]')
+      .last()
+      .should('contain.text', 'How to your dragon 2')
+  })
+})
